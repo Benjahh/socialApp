a9a54ccf3d867196d45103d7234e9bf7fc754c53
Lucas Agüero <67506785+Benjahh@users.noreply.github.com>
2023-05-31 17:27:53 +0000

    work 18

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -35,8 +35,50 @@ const Navbar = () => {
     const theme = useTheme()
     const neutralLight = theme.palette.neutral.light
     const dark = theme.palette.neutral.dark
+    const background = theme.palette.background.default
+    const primaryLight = theme.palette.background.light
+    const alt = theme.palette.background.alt
 
-    return <div> Navbar </div>
+    const fullName = `${user.firstName} ${user.lastName}`
+
+    return (
+       <div>
+         <FlexBetween padding="1rem 6%" backgroundColor={alt}>
+            <FlexBetween gap="1.75rem">
+                <Typography
+                fontWeight="bold"
+                fontSize="clamp(1rem, 2rem, 2.25rem"
+                color="primary"
+                onClick={()=> navigate("/home")}
+                sx={{
+                    "&:hover": {
+                        color: primaryLight,
+                        cursor: pointer
+                    }
+                }}
+                >
+                    SocialApp
+                </Typography>
+                { isNonMobileScreens && (
+                    <FlexBetween backgroundColor={neutralLight} borderRadius="9" gap="3rem" padding="0.1rem 1.5rem">
+                        <InputBase placeholder="Search...">
+                            <IconButton>
+                                <Search/>
+                            </IconButton>
+                        </InputBase>
+                    </FlexBetween>
+                )}
+            </FlexBetween>
+            { isNonMobileScreens ? (
+            <FlexBetween gap="2rem">
+                <IconButton onClick={() => dispatch(setMode())}></IconButton>
+            </FlexBetween>
+            ) : (
+                <IconButton></IconButton>
+            )}
+        </FlexBetween>
+       </div>
+    )
 
     
 }
