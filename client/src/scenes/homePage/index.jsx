3797239cb2a8aa76d1c23551e865242740eaef9f
Lucas Agüero <67506785+Benjahh@users.noreply.github.com>
2023-06-01 16:48:05 +0000

    work 21

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -1,6 +1,12 @@
+import { Box } from "@mui/material"
+import Navbar from "./scenes/navbar"
+
 export const HomePage = () => {
     return (
         <div>
+            <Box>
+                <Navbar></Navbar>
+            </Box>
 
         </div>
     )
